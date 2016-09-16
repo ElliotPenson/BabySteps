5f571b3277986e9340b6c35edf9d3c92835c94d9
Elliot Penson <elliotpenson@gmail.com>
2016-09-16 13:36:15 -0500

    Use spaces instead of tabs

--- src/babysteps/Task.java
+++ src/babysteps/Task.java
@@ -10,63 +10,63 @@ import java.util.ArrayList;
  * @author Elliot Penson
  */
 public class Task implements java.io.Serializable {
-	
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Name of the task.
-	 */
-	private String title;
-	
-	/**
-	 * Flag to indicate if the task has been finished.
-	 */
-	private boolean completed;
-	
-	/**
-	 * List of children that represent jobs within this task.
-	 */
-	private ArrayList<Task> subtasks;
-	
-	public Task(String title) {
-		this.title = title;
-		completed = false;
-		subtasks = new ArrayList<Task>();
-	}
+    private static final long serialVersionUID = 1L;
 
-	public String getTitle() {
-		return title;
-	}
-	
-	public void setTitle(String newTitle) {
-		title = newTitle;
-	}
-	
-	public boolean completed() {
-		return completed;
-	}
-	
-	public void setCompleted() {
-		completed = true;
-	}
-	
-	public void setUncompleted() {
-		completed = false;
-	}
-	
-	public void addSubtask(Task subtask) {
-		subtasks.add(subtask);
-	}
-	
-	public int numberOfSubtasks() {
-		return subtasks.size();
-	}
-	
-	public boolean hasSubtasks() {
-		return !subtasks.isEmpty();
-	}
-	
-	public ArrayList<Task> getSubtasks() {
-		return subtasks;
-	}
+    /**
+     * Name of the task.
+     */
+    private String title;
+
+    /**
+     * Flag to indicate if the task has been finished.
+     */
+    private boolean completed;
+
+    /**
+     * List of children that represent jobs within this task.
+     */
+    private ArrayList<Task> subtasks;
+
+    public Task(String title) {
+        this.title = title;
+        completed = false;
+        subtasks = new ArrayList<Task>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String newTitle) {
+        title = newTitle;
+    }
+
+    public boolean completed() {
+        return completed;
+    }
+
+    public void setCompleted() {
+        completed = true;
+    }
+
+    public void setUncompleted() {
+        completed = false;
+    }
+
+    public void addSubtask(Task subtask) {
+        subtasks.add(subtask);
+    }
+
+    public int numberOfSubtasks() {
+        return subtasks.size();
+    }
+
+    public boolean hasSubtasks() {
+        return !subtasks.isEmpty();
+    }
+
+    public ArrayList<Task> getSubtasks() {
+        return subtasks;
+    }
 }
